9eca31b3ea716317b292da566a3b5a11711e7004
Memo151515 <mehmetcanyildirim15@gmail.com>
2026-07-06 02:56:07 +0300

    Add Microsoft Edge vendor staging script

---
+++ packages/microsoft-edge/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+set -eu
+
+: "${SRC_TARBALL:?set SRC_TARBALL to the reviewed Microsoft Edge vendor archive}"
+BUILDDIR="${BUILDDIR:-build}"
+PKGDIR="${PKGDIR:-pkg}"
+PREFIX="${PREFIX:-/usr}"
+
+rm -rf "$BUILDDIR" "$PKGDIR"
+mkdir -p "$BUILDDIR" "$PKGDIR"
+case "$SRC_TARBALL" in
+  *.deb) ar x "$SRC_TARBALL" --output "$BUILDDIR" && tar -xf "$BUILDDIR"/data.tar.* -C "$PKGDIR" ;;
+  *.rpm) bsdtar -xf "$SRC_TARBALL" -C "$PKGDIR" ;;
+  *.tar.*|*.tgz) tar -xf "$SRC_TARBALL" -C "$PKGDIR" ;;
+  *) echo "Unsupported Edge payload format: $SRC_TARBALL" >&2; exit 1 ;;
+esac
+mkdir -p "$PKGDIR$PREFIX/bin"
+[ -e "$PKGDIR/opt/microsoft/msedge/microsoft-edge" ] && ln -sf /opt/microsoft/msedge/microsoft-edge "$PKGDIR$PREFIX/bin/microsoft-edge" || true
