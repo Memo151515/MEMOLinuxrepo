bc9f6c1c66a00cda95eee3197c3f302cd2a703d0
Memo151515 <mehmetcanyildirim15@gmail.com>
2026-07-06 02:58:22 +0300

    Add Links build script

---
+++ packages/links/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+set -eu
+
+: "${SRC_TARBALL:?set SRC_TARBALL to the Links source archive}"
+BUILDDIR="${BUILDDIR:-build}"
+PKGDIR="${PKGDIR:-pkg}"
+PREFIX="${PREFIX:-/usr}"
+
+rm -rf "$BUILDDIR" "$PKGDIR"
+mkdir -p "$BUILDDIR" "$PKGDIR"
+tar -xf "$SRC_TARBALL" -C "$BUILDDIR" --strip-components=1
+cd "$BUILDDIR"
+
+./configure --prefix="$PREFIX"
+make
+make DESTDIR="$(cd ../"$PKGDIR" && pwd)" install
