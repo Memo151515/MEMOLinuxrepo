0c2e97d4d56783113ca23bb69f29fb30509c4ac6
Memo151515 <mehmetcanyildirim15@gmail.com>
2026-07-06 02:58:20 +0300

    Add w3m build script

---
+++ packages/w3m/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+set -eu
+
+: "${SRC_TARBALL:?set SRC_TARBALL to the w3m source archive}"
+BUILDDIR="${BUILDDIR:-build}"
+PKGDIR="${PKGDIR:-pkg}"
+PREFIX="${PREFIX:-/usr}"
+
+rm -rf "$BUILDDIR" "$PKGDIR"
+mkdir -p "$BUILDDIR" "$PKGDIR"
+tar -xf "$SRC_TARBALL" -C "$BUILDDIR" --strip-components=1
+cd "$BUILDDIR"
+
+./configure --prefix="$PREFIX"
+make
+make DESTDIR="$(cd ../"$PKGDIR" && pwd)" install
