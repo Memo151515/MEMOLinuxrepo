8e1709cb13c8ded558866f6dbac6b45f3434d4e7
Memo151515 <mehmetcanyildirim15@gmail.com>
2026-07-06 02:54:23 +0300

    Add LibreWolf build script scaffold

---
+++ packages/librewolf/build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+set -eu
+
+: "${SRC_TARBALL:?set SRC_TARBALL to the LibreWolf source archive}"
+BUILDDIR="${BUILDDIR:-build}"
+PKGDIR="${PKGDIR:-pkg}"
+PREFIX="${PREFIX:-/usr}"
+
+rm -rf "$BUILDDIR" "$PKGDIR"
+mkdir -p "$BUILDDIR" "$PKGDIR"
+tar -xf "$SRC_TARBALL" -C "$BUILDDIR" --strip-components=1
+cd "$BUILDDIR"
+
+if [ -x ./mach ]; then
+  ./mach build
+  DESTDIR="$(cd ../"$PKGDIR" && pwd)" ./mach install
+else
+  echo "LibreWolf source layout changed; update MEMO recipe after pinning release." >&2
+  exit 1
+fi
