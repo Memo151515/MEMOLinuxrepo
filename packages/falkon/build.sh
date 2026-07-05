afe9750621a348f47f77fae984f765c1be9a4f3f
Memo151515 <mehmetcanyildirim15@gmail.com>
2026-07-06 02:56:53 +0300

    Add Falkon build script

---
+++ packages/falkon/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+set -eu
+
+: "${SRC_TARBALL:?set SRC_TARBALL to the Falkon source archive}"
+BUILDDIR="${BUILDDIR:-build}"
+PKGDIR="${PKGDIR:-pkg}"
+PREFIX="${PREFIX:-/usr}"
+
+rm -rf "$BUILDDIR" "$PKGDIR"
+mkdir -p "$BUILDDIR" "$PKGDIR"
+tar -xf "$SRC_TARBALL" -C "$BUILDDIR" --strip-components=1
+cd "$BUILDDIR"
+
+cmake -S . -B memo-build -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX"
+cmake --build memo-build
+DESTDIR="$(cd ../"$PKGDIR" && pwd)" cmake --install memo-build
