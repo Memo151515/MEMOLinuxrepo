ef34dd0070b5a86c1673db6f370c8ae88e2fe9a7
Memo151515 <mehmetcanyildirim15@gmail.com>
2026-07-14 21:13:13 +0300

    Update build.sh

--- packages/cmatrix/build.sh
+++ packages/cmatrix/build.sh
@@ -5,8 +5,12 @@ set -e
 tar -xf "$SRC_TARBALL" -C "$BUILDDIR" --strip-components=1
 cd "$BUILDDIR"
 
-# 2. CMake ile yapılandırıp derliyoruz
-cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="/usr/local"
+# 2. CMake ile uyumluluk parametresini ekleyerek yapılandırıp derliyoruz
+cmake -S . -B build \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX="/usr/local" \
+  -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+
 cmake --build build
 
 # 3. Dosyaları paketleme (hazırlık) dizinine kuruyoruz
